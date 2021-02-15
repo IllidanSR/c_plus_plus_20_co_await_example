cff0d7dfae925e7ccc5145ea2e504a590c4f6d55
illidansr <sergei.matyshev@lacit.net>
2021-02-15 17:30:30 +0300

    feat : add stop coroutine

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ class evt_awaiter_t {
     std::map<int, awaiter> awaiter_map;
     bool set_;
     std::atomic_int index = 0;
+    int local_id = 0;
     struct awaiter {
         evt_awaiter_t &event_;
         coro_t coro_ = nullptr;
@@ -41,10 +42,12 @@ public:
 public:
     bool is_set() const noexcept { return set_; }
     void push_awaiter(awaiter a) { lst_.push_back(a); }
+    void set_id(int id) {
+        local_id = id;
+    }
     int add_awaiter(awaiter a) {
-        int ind = index++;
-        awaiter_map.insert(std::pair<int, awaiter>(ind, a));
-        return ind;
+        awaiter_map.insert(std::pair<int, awaiter>(local_id, a));
+        return local_id;
     }
 
     awaiter operator co_await() noexcept { return awaiter{*this}; }
@@ -53,24 +56,19 @@ public:
         auto corout = awaiter_map.find(ind);
         corout->second.coro_.resume();
     }
-    void set() noexcept {
-        set_ = true;
-#if INEFF
-        auto ntoresume = lst_.size();
-    while (ntoresume > 0) {
-      lst_.front().coro_.resume();
-      lst_.pop_front();
-      ntoresume--;
+
+    void delete_from_map(int idn) {
+        auto corout = awaiter_map.find(idn);
+        corout->second.coro_.done();
     }
-#else
-        std::list<awaiter> toresume;
-        toresume.splice(toresume.begin(), lst_);
-        for (auto s: toresume)
-            s.coro_.resume();
-#endif
+    int size() {
+       return awaiter_map.size();
     }
 
-    void reset() noexcept { set_ = false; }
+    void reset() noexcept {
+        set_ = false;
+    }
+
 };
 
 class SubClassTest {
@@ -88,7 +86,7 @@ struct resumable {
         using coro_handle = std::coroutine_handle<promise_type>;
         auto get_return_object() { return coro_handle::from_promise(*this); }
         auto initial_suspend() { return std::suspend_always(); }
-        auto final_suspend() { return std::suspend_always(); }
+        auto final_suspend() { return std::suspend_never(); }
         void return_void() {}
         void unhandled_exception() { std::terminate(); }
     };
@@ -171,31 +169,39 @@ int g_value;
 evt_awaiter_t g_evt;
 
 void producer(int value) {
-    std::cout << "Producer started" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    g_value = 42;
-    std::cout << "Value ready" << std::endl;
-    g_evt.set_to_map(value);
+    if (value == 99) {
+        g_evt.delete_from_map(900);
+    } else {
+        std::cout << "Producer started" << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        g_value = 42;
+        std::cout << "Value ready" << std::endl;
+        g_evt.set_to_map(value);
+    }
+
 }
 
 resumable_no_own consumer1() {
     std::cout << "Consumer1 started" << std::endl;
+    g_evt.set_id(700);
     co_await g_evt;
     std::cout << "Consumer1 resumed" << std::endl;
 }
 
 resumable_no_own consumer2() {
     std::cout << "Consumer2 started" << std::endl;
+    g_evt.set_id(800);
     co_await g_evt;
     std::cout << "Consumer2 resumed" << std::endl;
 }
 
 resumable_no_own consumer3(int val) {
     std::cout << "Consumer3 started" << std::endl;
+    g_evt.set_id(900);
     co_await g_evt;
-    std::cout << "Consumer3 resumed" << std::endl;
+    std::cout << "Consumer3 resumed : " << val << std::endl;
     co_await g_evt;
-    std::cout << "Consumer3 resumed again" << std::endl;
+    std::cout << "Consumer3 resumed again : " << val << std::endl;
 }
 
 int main() {
